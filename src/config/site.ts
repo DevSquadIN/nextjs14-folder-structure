6d305874d30ba50c752f7d6944453f61c736e77b
Joy Poddar <joypoddar.sd@gmail.com>
2023-12-20 22:20:35 +0530

    init: initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Folder Structure",
+  description: "NextJS 14 Enterprise Folder Structure",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+}
